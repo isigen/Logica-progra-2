546e3647c455a14dd99b228bbc43f2ce911278f4
isigen <isidora.abarzua@genstudents.org>
2024-05-09 15:41:01 -0400

    añadiendo una alerta con un while!

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let celcius = parseFloat(prompt("por favor introduzca sus grados celcius: "))
+let celcius //= parseFloat(prompt("por favor introduzca sus grados celcius: "))
+while (true) {
+    celcius = parseFloat(prompt("Ingresa la temperatura en grados Celsius:"));
+    if (!isNaN(celcius)) {
+        break;
+    } else {
+        alert("¡Error! Por favor, ingresa un número válido.");
+    }
+}
+
 console.log(`Convirtiendo tus grados Celsius a Fahrenheit y Kelvin...`);
 
 
 let conviertiendo = function(celcius){
-    if (isNaN(celcius)){
-        return "error!, vuelve  introducir tus NUMEROS";
-    }
+    // if (isNaN(celcius)){
+    //     return "error!, vuelve  introducir tus NUMEROS";
+    // }  quitamos este y el de abajo para probar una alerta y un while más arriba.
     Fharenheit = (celcius*1.8)+32
     Kelvin = (celcius+273.15)
     
@@ -15,12 +24,12 @@ let conviertiendo = function(celcius){
             Kelvi : Kelvin
         }  
     }
-    
+
 
 let resultado = conviertiendo(celcius)
-if (typeof resultado === "string") {
-    console.log(resultado);
-}else{
+// if (typeof resultado === "string") {
+//     console.log(resultado);
+// }else{  comentado, explicacion arriba!!
      console.log("Aqui tienes tus celcius en Fharenheit!!", resultado.Faren)
      console.log("Aqui tienes tus resultados en Kelvin!!", resultado.Kelvi)
-}
+// }
